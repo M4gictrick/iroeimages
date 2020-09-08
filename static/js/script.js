1feeb6e82db467dcb3779a8fa8504080037f254e
M4gictrick <chonlasit_jee@hotmail.co.th>
2020-09-08 17:34:03 +0700

    fix txt&vid2

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+
+    function start()
+    {
+    setPlaySpeed1();
+    setPlaySpeed2();
+    }
+
+var vid1 = document.getElementById("vid1");
+    function setPlaySpeed1(){
+        vid1.playbackRate = 0.5;
+    }
+
+var vid2 = document.getElementById("vid2");
+    function setPlaySpeed2(){
+        vid2.playbackRate = 0.5;
+    }
